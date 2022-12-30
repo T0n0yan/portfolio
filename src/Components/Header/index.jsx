b8d258d165e95af29d3bf9f726187d60b314f5c8
Davit <t0n0yan@internet.ru>
2022-12-31 00:40:49 +0400

    fixed social box animation

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,70 +1,105 @@
-import React, {useState} from 'react';
-import './style.scss'
-import {NavLink, useLocation} from "react-router-dom";
-import logo from '../../assets/images/logo2.svg';
-import hoverSoundMenu from '../../assets/sounds/hover.mp3';
-import logoSound from '../../assets/sounds/checkpoint.mp3';
-import {useTranslation} from "react-i18next";
-import '../../i18n/i18next'
-
+import React, { useState } from "react";
+import "./style.scss";
+import { NavLink, useLocation } from "react-router-dom";
+import logo from "../../assets/images/logo2.svg";
+import hoverSoundMenu from "../../assets/sounds/hover.mp3";
+import logoSound from "../../assets/sounds/checkpoint.mp3";
+import { useTranslation } from "react-i18next";
+import "../../i18n/i18next";
 
 const Header = () => {
-    const {t,i18n} = useTranslation()
-    const location = useLocation()
-    const [click, setClick] = useState(new Audio(hoverSoundMenu))
-    const [checkSound, setCheckSound] = useState(new Audio(logoSound))
-    const [menu, setMenu] = useState(false)
+  const { t, i18n } = useTranslation();
+  const location = useLocation();
+  const [click, setClick] = useState(new Audio(hoverSoundMenu));
+  const [checkSound, setCheckSound] = useState(new Audio(logoSound));
+  const [menu, setMenu] = useState(false);
 
-    const handleClick = () => {
-        setMenu(!menu)
-    }
-    const logoSoudeOn = () => {
-        checkSound.play()
-    }
-    const handleMouseHover = () => {
-        click.play()
-    }
-    const changeLanguage = (lang) => {
-        i18n.changeLanguage(lang)
-    }
-    return (
-        <>
-            <div className='L-burger G-flex' onClick={handleClick}>
-                <span className={`burger-line ${menu ? 'burger-active' : ''}`}/>
-            </div>
-            <header className={`L-header-container ${menu ? 'L-header-active' : ''} `}>
-                <div className='G-container L-header-wrapper G-flex G-justify-between G-align-center'>
-                    <div className='L-logo-container ' onClick={() => logoSoudeOn()}>
-                        <NavLink to='/home' className='L-logo G-background-contain'
-                                 style={{backgroundImage: `url('${logo}')`}}>
-                        </NavLink>
-                    </div>
-                    <nav className='L-nav-container  G-flex G-justify-end '>
-                        <ul className='L-nav-lists G-flex G-align-center G-justify-between '>
-                            <li onClick={() => handleMouseHover()}>
-                                <NavLink to='/about'>{t('About Me')}</NavLink>
-                            </li>
-                            <li onClick={() => handleMouseHover()}>
-                                <NavLink to='/skills'>Skills</NavLink></li>
-                            <li onClick={() => handleMouseHover()}>
-                                <NavLink to='/projects'>Projects</NavLink></li>
-                            <li onClick={() => handleMouseHover()}>
-                                <NavLink to='/contacts'>Contacts</NavLink></li>
-                        </ul>
-                    </nav>
-                    <div className='L-languages-cont G-flex  G-align-center G-justify-center G-flex-column'>
-                        <select>
-                            <option value="Armenia" onChange={()=> changeLanguage('am')}>HY</option>
-                            <option value="Russia" onChange={()=> changeLanguage('ru')}>RU</option>
-                            <option value="English" onChange={()=> changeLanguage('en')}>EN</option>
-                        </select>
-                    </div>
-                </div>
-            </header>
-        </>
+  const handleClick = () => {
+    setMenu(!menu);
+  };
+  const logoSoudeOn = () => {
+    checkSound.play();
+  };
+  const handleMouseHover = () => {
+    click.play();
+  };
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
 
-
-    );
+  return (
+    <>
+      <div className="L-burger G-flex" onClick={handleClick}>
+        <span className={`burger-line ${menu ? "burger-active" : ""}`} />
+      </div>
+      <header
+        className={`L-header-container ${menu ? "L-header-active" : ""} `}
+      >
+        <div className="G-container L-header-wrapper G-flex G-justify-between G-align-center">
+          <div className="L-logo-container " onClick={() => logoSoudeOn()}>
+            <NavLink
+              to="/home"
+              className="L-logo G-background-contain"
+              style={{ backgroundImage: `url('${logo}')` }}
+            ></NavLink>
+          </div>
+          <nav className="L-nav-container  G-flex G-justify-end ">
+            <ul className="L-nav-lists G-flex G-align-center G-justify-between ">
+              <li onClick={() => handleMouseHover()}>
+                <NavLink to="/about" onClick={() => setMenu(false)}>
+                  {t("About Me")}
+                </NavLink>
+              </li>
+              <li onClick={() => handleMouseHover()}>
+                <NavLink to="/skills" onClick={() => setMenu(false)}>
+                  Skills
+                </NavLink>
+              </li>
+              <li onClick={() => handleMouseHover()}>
+                <NavLink to="/projects" onClick={() => setMenu(false)}>
+                  Projects
+                </NavLink>
+              </li>
+              <li onClick={() => handleMouseHover()}>
+                <NavLink to="/contacts" onClick={() => setMenu(false)}>
+                  Contacts
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="L-languages-cont G-flex  G-align-center G-justify-center G-flex-column">
+            <select>
+              <option
+                value="Armenia"
+                onChange={() => {
+                  changeLanguage("am");
+                }}
+                onClick={() => {
+                  setMenu(false);
+                  console.log("ru hayeren");
+                }}
+              >
+                HY
+              </option>
+              <option
+                value="Russia"
+                onChange={() => changeLanguage("ru")}
+                onClick={() => {
+                  setMenu(false);
+                  console.log(menu);
+                }}
+              >
+                RU
+              </option>
+              <option value="English" onChange={() => changeLanguage("en")}>
+                EN
+              </option>
+            </select>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
--- src/Pages/projects/index.jsx
+++ src/Pages/projects/index.jsx
@@ -1,34 +1,78 @@
-import React from 'react';
-import './style.scss'
-import image from '../../assets/images/Reload Logo.png'
+import React from "react";
+import "./style.scss";
+import image from "../../assets/images/Reload Logo.png";
+import Slider from "react-slick";
+
+
+const projectsSettings = {
+    dots: true,
+    infinite: false,
+    speed: 300,
+    arrows: true,
+    slidesToShow: 1,
+    autoplay: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: "40px",
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                centerMode: true,
+                centerPadding: "40px",
+                slidesToShow: 1,
+            },
+        },
+    ],
+};
+
+
 const Projects = () => {
     return (
-        <div className='L-projects-wrapper'>
+        <div className="L-projects-wrapper">
             <div className="G-container">
-                <div className="projects-container G-flex">
-                    <div className="card ">
-                        <div className="front">
-                            <img src={image} alt='Rockstar logo'/>
+                {/*<Slider {...projectsSettings} >*/}
+                    <div className="L-projects-container G-flex G-align-center G-justify-between G-flex-wrap">
+                        <div className="card  G-text-align">
+                            <div className="front">
+                                <img src={image} alt="Rockstar logo"/>
+                            </div>
+                            <div className="back">
+                                <div className="back-content ">
+                                    <h2>Barev Bro</h2>
+                                </div>
+                            </div>
                         </div>
-                        <div className="back">
-                            <div className="back-content ">
-                                <h2>Barev Bro</h2>
+                        <div className="card  G-text-align">
+                            <div className="front">
+                                <img src={image} alt="Rockstar logo"/>
+                            </div>
+                            <div className="back">
+                                <div className="back-content ">
+                                    <h2>Barev Bro</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card  G-text-align">
+                            <div className="front">
+                                <img src={image} alt="Rockstar logo"/>
+                            </div>
+                            <div className="back">
+                                <div className="back-content ">
+                                    <h2>Barev Bro</h2>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="card ">
-                        <div className="front">
-                            <img src={image} alt='Rockstar logo'/>
-                        </div>
-                        <div className="back">
-                            <div className="back-content ">
-                                <h2>Barev Bro</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                {/*</Slider>*/}
 
+            </div>
         </div>
     );
 };
