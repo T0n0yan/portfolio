a3a54a25f5e4c3cba5509211408d392b1630c80c
Davit <t0n0yan@internet.ru>
2023-01-10 02:16:26 +0400

    making slider for projects

--- src/Pages/projects/index.jsx
+++ src/Pages/projects/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./style.scss";
 import Slider from "react-slick";
+import {ProjectsData} from "./projects.data";
 
-const ssss = {
+
+const projects_slider = {
     dots: false,
     infinite: true,
     speed: 300,
@@ -11,20 +13,15 @@ const ssss = {
     adaptiveHeight: true,
     autoplay: true,
     autoplaySpeed: 3000,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: false,
-                centerPadding: "40px",
-                slidesToShow: 1,
-            },
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 1,
         },
+    },
         {
             breakpoint: 480,
             settings: {
-                centerMode: false,
                 slidesToShow: 1,
             },
         },
@@ -37,18 +34,33 @@ const Projects = () => {
     return (<div className="L-projects-wrapper">
         <div className="G-container">
             <div className="L-projects-container G-flex G-align-center G-justify-between G-flex-wrap">
-                <div className="card  G-text-align">
-                    <div className="front G-background-cover">
-                        <Slider {...ssss} className="L-projects-slider ">
-
-                        </Slider>
-                    </div>
-                    <div className="back">
-                        <div className="back-content ">
-                            <h2>Barev Bro</h2>
-                        </div>
-                    </div>
-                </div>
+                {ProjectsData.map((el) => {
+                    return (
+                        <div key={el.id} className="card G-flex G-flex-wrap G-text-align">
+                            <div className="front   G-background-cover">
+                                <Slider {...projects_slider} className="L-projects-slider  ">
+                                    {ProjectsData.map(() => {
+                                        return (
+                                            el.img.map((i) => {
+                                                return (<div key={i.id} style={{width: '500px', height: '300px'}}>
+                                                    <img src={i.img}
+                                                         style={{
+                                                             width: '100%',
+                                                             height: '300px',
+                                                             border: '1px solid red',
+                                                         }}/>
+                                                </div>)
+                                            }))
+                                    })}
+                                </Slider>
+                            </div>
+                            <div className="back">
+                                <div className="back-content ">
+                                    <h2>{el.description}</h2>
+                                </div>
+                            </div>
+                        </div>)
+                })}
             </div>
 
         </div>
--- src/Pages/projects/projects.data.js
+++ src/Pages/projects/projects.data.js
@@ -1,12 +1,54 @@
+import {ProjectsImagesData} from "../../assets/images/Projects Images/projectsImagesData";
 
 
 export const ProjectsData = [
     {
         id: 1,
-        img: {
-            img1: '',
-            img2: ''
-        }
+        name: 'ZayShop',
+        img: [{
+            id: 1, img: ProjectsImagesData.zayshop_home,
+        }, {
+            id: 2, img: ProjectsImagesData.zayshop_about,
+        }, {
+            id: 3, img: ProjectsImagesData.zayshop_footer
+        }, {
+            id: 4, img: ProjectsImagesData.zayshop_shop,
+        }, {
+            id: 5, img: ProjectsImagesData.zayshop_product1,
+        }, {
+            id: 6, img: ProjectsImagesData.zayshop_product2
+        }],
+        description: 'sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss',
+        link: ''
     },
-    {}
+    {
+        id: 2,
+        name: 'admin',
+        img: [{
+            id: 7, img: ProjectsImagesData.admin_login
+        }, {
+            id: 8, img: ProjectsImagesData.admin_home,
+        }, {
+            id: 9, img: ProjectsImagesData.admin_registration,
+        }, {
+            id: 10, img: ProjectsImagesData.admin_product,
+        }, {
+            id: 11, img: ProjectsImagesData.admin_crud,
+        },],
+        description: 'anush axper',
+        link: ''
+
+    },
+    {
+        id: 3,
+        name: 'todo',
+        img: [{
+            id: 12,
+            img: ProjectsImagesData.todo_main
+        }],
+        description: 'Crud',
+        link: ''
+
+    }
+
 ]
